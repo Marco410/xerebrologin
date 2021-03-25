d7e6b29b0de44e814986d244b5f66b457e7f7352
Marco410 <47765796+Marco410@users.noreply.github.com>
2021-03-25 11:40:18 -0600

    Update settings.py

--- analisis/settings.py
+++ analisis/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
 #Auth redirects
 
 LOGIN_REDIRECT_URL = reverse_lazy('home')
